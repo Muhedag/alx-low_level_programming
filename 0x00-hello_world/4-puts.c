3556860075cd73fb0f1c04383b5c4f9147341b0a
muhedag <madaau2002@gmail.com>
2023-02-16 19:28:21 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - print a line using puts finction
+ * Return: 0 (sucess)
  */
 int main(void)
-{ 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
