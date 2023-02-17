049432b4fbc780bd53a58555a74ffaf1705adf07
muhedag <madaau2002@gmail.com>
2023-02-16 18:58:55 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line  with purs function
- * return: 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
-{
+{ 
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
