7091a69bfdb6e8ee6d5a4207edbb8ebdb8d86633
muhedag <madaau2002@gmail.com>
2023-02-20 05:36:02 -0800

    Unix task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program prints a line
+ * Description: It is c language
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+
+}
