cb03c99482345dc157ffb44d99670019bea866c0
muhedag <madaau2002@gmail.com>
2023-02-20 16:54:43 -0800

    update on task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,14 +4,14 @@
  * Description: Lowere the upper case
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
- int i;
- for (i = 0; i < 52; i++)
- {
-	 putchar(alp[i]);
-	
- }
- putchar('\n');
- return (0);
+int i;
+
+for (i = 0; i < 52; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
 }
