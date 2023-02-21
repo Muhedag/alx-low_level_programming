32529d8c4a388e10f561a476f8f17d4b065255c0
muhedag <madaau2002@gmail.com>
2023-02-20 17:07:01 -0800

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,12 @@
  * Return: 0
  */
 int main(void)
+{int i;
+
+for (i = 0; i < 10; i++)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-	print("%d", i);
-	}
-	putchar('\n');
-	return (0);
+printf("%d", i);
+}
+putchar('\n');
+return (0);
 }
