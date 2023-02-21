b92bb3b943702865bb5754edd01ac4755dd43072
muhedag <madaau2002@gmail.com>
2023-02-20 18:12:53 -0800

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry program
- * Description : possible combination of two digits
- * Return : 0
+ * Description: possible combination of two digits
+ * Return: 0
  */
 int main(void)
 {int n, m;
@@ -12,11 +12,13 @@ for (n = 48; n <= 56; n++)
 for (m = 49; m <= 57; m++)
 {
 if (m > n)
+{
 putchar(n);
 putchar(m);
 if (n != 56 || m != 57)
 {putchar(',');
 putchar(' ');
+}
 }
 }
 }
